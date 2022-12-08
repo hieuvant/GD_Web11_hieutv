c75e61ee23453a3516ab13f898be11f3d595e10a
hieuvant <tranhieust24052@gmail.com>
2022-12-08 08:44:58 +0700

    Đổ dữ liệu vào table bằng js

--- js/index.js
+++ js/index.js
@@ -6,55 +6,54 @@ createEvent();
  * Author: TVHieu - MF1485 (5/12/2022)
  * EditBy: TVHieu - MF1485 (7/12/2022) - Sửa lại Api
 */
+"use strict";
+// GOI API LAY DU LIEU
 function loadData() {
-    // Gọi api lấy dữ liệu
-    $.ajax({
-        type: "GET",
-        url: "https://amis.manhnv.net/api/v1/Employees",
-        dataType: "dataType",
-        success: function (response) {
-            console.log(response);
-            // xử lý dữ liệu
-            //Hiển thị dữ liệu lên table:
-            $("#tbIndexList tbody").empty();
-            for (const emp of response){
-            var employeeCode = emp.EmployeeCode;
-            var fullName = emp["EmployeeCode"];
-            var phoneNumber = emp.PhoneNumber;
-            var trHTML = 
-            `<tr>
-            <td class="number-marin">
-                    <input type="checkbox"
-                    class="checkbox-table">
-                </td>
-                <td class="number-marin">${employeeCode}</td>
-                <td class="number-left-name">${fullName}</td>
-                <td class="number-marin">${phoneNumber}</td>
-                <td class="number-left">Tổ Toán - Tin</td>
-                <td class="number-left">Toán</td>
-                <td class="number-left">Phòng tin học</td>
-                <td class="number-marin">
-                 <div class="table-checklist"></div>
-                </td>
-                <td class="number-marin">
-                    <div class="table-checklist"></div>
-                </td>
-                <td class="number-marin">
-                    <button class="btn-edit"></button>
-                    <button class="btn-remove"></button>
-                </td>
-            </tr>`;
-            $("#tbEmployeeList tbody").append(trHTML);
-            }
-        },
-        error: function(error){
-            console.log(error);
-        }
-    });
+  $.ajax({
+    type: "GET",
+    url: "https://amis.manhnv.net/api/v1/Employees",
+    success(respone) {
+      console.log(respone);
+      // xử lý dữ liệu
+      // Hiển thị dữ liệu lên table:
+      for (const emp of respone) {
+        const employeeCode = emp.EmployeeCode;
+        const employeeName = emp.EmployeeName;
+        const employeePhone = emp.TelephoneNumber;
+        const employeeDepartment = emp.DepartmentName;
+        const trHTML = `<tr>
+      <td class="number-marin">
+          <input type="checkbox" class="checkbox-table">
+      </td>
+      <td class="number-marin">${employeeCode || ""}</td>
+      <td class="number-left-name">${employeeName || ""}</td>
+      <td class="number-marin">${employeePhone || ""}</td>
+      <td class="number-left">${employeeDepartment || ""}</td>
+      <td class="number-left">${employeeDepartment || ""}</td>
+      <td class="number-left">${employeeDepartment || ""}</td>
+      <td class="number-marin">
+          <div class="table-checklist"></div>
+      </td>
+      <td class="number-marin">
+          <div class="table-checklist"></div>
+      </td>
+      <td class="number-marin">
+          <button class="btn-edit"></button>
+          <button class="btn-remove"></button>
+      </td>
+  </tr>`;
+        $("#tb-employee-list").append(trHTML);
+      }
+    },
+    error(error) {
+      console.log(error);
+    },
+  });
 
-    // Hanlde lỗi từ Api (nếu có)
+  // Hanlde lỗi từ Api (nếu có)
 
 }
+
 //Lập trình cho các sự kiện:
 function createEvent() {
 
