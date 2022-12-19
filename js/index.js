bef66ef63ff68610c25f951846c65cff7305c2f2
hieuvant <tranhieust24052@gmail.com>
2022-12-19 18:27:28 +0700

    chức năng thêm trong popup

--- js/index.js
+++ js/index.js
@@ -64,6 +64,10 @@ function initEvents() {
   document.getElementById('btnAdd').addEventListener("click", btnAdd);
   //Button close
   document.getElementById('btnDialogClose').addEventListener("click", btnDialogClose);
+  //Close toast_msg error
+  document.getElementById('toastMsgClose').addEventListener("click", bntCloseErrorMessage);
+  //Close toast_msg succes
+  document.getElementById('toastMsgSuccesClose').addEventListener("click", bntCloseSuccesMessage);
 }
 
 function btnDialogClose() {
@@ -84,76 +88,168 @@ function btnAdd() {
         $("#txtEmployeeCode").focus();
       }
     });
-  } catch (error) {
 
+    //forcus vào ô nhập liệu đầu tiên
+    $("#txtEmployeeCode").focus();
+  } catch (error) {
+    console.log("error");
   }
 }
 
 /** 
  * Thực hiện lưu dữ liệu
- * Author: TVHieu - MF1485 (8/12/2022)
+  * Author: TVHieu - MF1485 (15/12/2022)
 */
-function createEvent(){
+function createEvent() {
   try {
     $("#btnSave").click(btnSaveOnclick);
+    $("#txtEmployeeCode").blur(onValidateFieldRequired1);
+    $("#txtEmployeeName").blur(onValidateFieldRequired2);
   } catch (error) {
     console.log(error);
   }
 }
 
+/**
+ * Đóng thông báo lỗi khi nhập sai thông tin cán bộ
+ * Author: TVHieu - MF1485 (8/12/2022)
+*/
+function bntCloseErrorMessage() {
+  //Ẩn toast msg
+  document.getElementById("toastMsg").style.display = "none";
+}
+
+/**
+ * Đóng thông báo khi thông tin cán bộ lưu thông tin cán bộ thành công
+ * Author: TVHieu - MF1485 (8/12/2022)
+*/
+function bntCloseSuccesMessage() {
+  //Ẩn toast msg
+  document.getElementById("toastMsgSucces").style.display = "none";
+}
+
+/**
+ * save dữ liệu
+ * Author: TVHieu - MF1485 (8/12/2022)
+ */
 function btnSaveOnclick() {
   try {
     // Thu thập dữ liệu
-    const employee = $("#txtEmployeeName").val(); //--> bắt buộc
+    const employeeName = $("#txtEmployeeName").val(); //--> bắt buộc
     const employeeCode = $("#txtEmployeeCode").val(); //--> bắt buộc
-    const email = $("#txtEmail").val(); //--> đúng định dạng
+    const mobie = $("#txtEmployeeMobie").val();
+    const email = $("#txtEmployeeEmail").val();
+    let errorMsgs = [];
+    errorFocus = [];
 
-    // Validate dữ liệu
-    if ((employeeCode.toString().trim() == '') || employeeCode == null || employeeCode == undefined) {
-      alert("Mã nhân viên không được để trống");
+    // Check xem dữ liệu bắt buộc đã nhập chưa
+    if (!employeeCode) {
+      errorMsgs.push("Số hiệu cán bộ không được phép để trống");
+      $("#txtEmployeeCode").addClass("input-error");
+      errorFocus.push($("#txtEmployeeCode"));
+      $("#txtEmployeeCode").hover(showError1);
+    } else {
+      $("#txtEmployeeCode").removeClass("input-error");
+    }
+    if (!employeeName) {
+      errorMsgs.push("Họ và tên không được phép để trống");
+      $("#txtEmployeeName").addClass("input-error");
+      errorFocus.push($("#txtEmployeeName"));
+      $("#txtEmployeeName").hover(showError2);
+    } else {
+      $("#txtEmployeeName").removeClass("input-error");
+    }
+    //Clear error msg
+    document.querySelector(".toast-message-content").innerHTML = "";
+    // Kiểm tra errorMsgs xem có lỗi không
+    if (errorMsgs.length > 0) {
+      // Nếu có lỗi thì hiển thị ra dialog báo lỗi
+      for (const errMsg of errorMsgs) {
+        $(".toast-message-content").append(`<div >${errMsg}</div>`);
+      }
+      $(".toast-message-container").css("display", "flex");
+    } else {
+      $.ajax({
+        type: "POST",
+        url: "https://amis.manhnv.net/api/v1/Employees",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+          employeeCode: `${employeeCode}`,
+          employeeName: `${employeeName}`,
+          email: `${email}`,
+          telephoneNumber: `${mobie}`,
+          departmentId: "142cb08f-7c31-21fa-8e90-67245e8b283e",
+        }),
+        success() {
+          alert("succes");
+          // document.getElementById("toastMsgSucces").style.display = "block";
+        },
+        error(error) {
+          console.log(error);
+        },
+      });
     }
 
-    if ((employee.toString().trim() == '') || employee == null || employee == undefined) {
-      alert("Tên nhân viên không được để trống");
-    }
-
-    if ((employeeCode.toString().trim() == '') || employeeCode == null || employeeCode == undefined) {
-      alert("Mã nhân viên không được để trống");
-    }
-    // // Gọi api thực hiện cất dữ liệu
-    // fetch("https://amis.manhnv.net/api/v1/Departments",
-    //   { method: "POST", body: JSON.stringify(employee) })
-    //   .then(res => res.json())
-    //   .then(res => {
-    //     console.log(res);
-    //   })
-    //   .catch(res => {
-
-    //   })
-    // // Kiểm tra két quả trả về -> đưa ra thông báo:
   } catch (error) {
 
   }
 
 }
 
-/** 
- * Thực hiện validate dữ liệu
- * Author: TVHieu - MF1485 (8/12/2022)
-*/
-// function validateData() {
-//   // Các thông tin bắt buộc phải nhập
-//   var employeeCode = document.getElementById("txtEmployeeCode").value;
-//   var fullname = document.getElementById("txtEmployeeName").value;
-//   if (employeeCode == '' || employeeCode === '' || employeeCode == null) {
-//     // Add border màu đỏ
-//     document.getElementById("txtEmployeeCode").style.border = "1px solid #ff0400";
-//     //Hiển thị thông báo ở bên dưới elêmnt
+/**
+ * Hàm khi điền xong thì cho ô input dialog trở về bình thường
+ * Author: TVHieu - MF1485 (15/12/2022)
+ */
+const onValidateFieldRequired1 = function () {
+  try {
+    const value = $("#txtEmployeeCode").val();
+    if (!value) {
+      $("#txtEmployeeCode").addClass("input-error");
+    } else {
+      $("#txtEmployeeCode").removeClass("input-error");
+      $(".dialog-el-2-error").hide();
+      $("#txtEmployeeCode").hover(function () {
+        $(".dialog-el-2-error").hide();
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const onValidateFieldRequired2 = function () {
+  try {
+    const value = $("#txtEmployeeName").val();
+    if (!value) {
+      $("#txtEmployeeName").addClass("input-error");
+    } else {
+      $("#txtEmployeeName").removeClass("input-error");
+      $(".dialog-el-0-error").hide();
+      $("#txtEmployeeName").hover(function () {
+        $(".dialog-el-0-error").hide();
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//   } else {
-//     document.getElementById("txtEmployeeCode").style.border = "1px solid #000";
-//   }
-//   // Các thông tin đúng định dạng (email)
-
-//   // Ngày tháng
-// }
+//Hàm để hover vào input là show ra lỗi không nhập số hiệu và họ tên
+const showError1 = function () {
+  try {
+    $(".dialog-el-2-error").toggle();
+    $(".dialog-el-0").css("position", "static");
+    $(".dialog-el-2").css("position", "relative");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const showError2 = function () {
+  try {
+    $(".dialog-el-0-error").toggle();
+    $(".dialog-el-0").css("position", "relative");
+    $(".dialog-el-2").css("position", "static");
+  } catch (error) {
+    console.log(error);
+  }
+};
